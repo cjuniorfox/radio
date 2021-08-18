ab2f9b6223951322e38dc198e38fcfe128f59fba
Carlos Junior <cjuniorfox@gmail.com>
2021-08-18 10:21:17 -0300

    README.md

---
+++ README.rm
@@ -0,0 +1,3 @@
+Compile FFMpeg. Do not forget the to enable the OpenSSL Option
+./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree && make && sudo make install
+
--- fm.py
+++ fm.py
@@ -11,7 +11,7 @@ rds_ctl = os.path.join('/tmp','rds_ctl')
 
 subprocess.Popen(["mkfifo",rds_ctl])
 rec = subprocess.Popen(["arecord", "-fS16_LE", "-r", "44100", "-Dplughw:0,0", "-c", "2", "-"],stdout=subprocess.PIPE)
-fm = subprocess.Popen(["pi_fm_rds","-ps", "","-freq" , freq, "-audio", "-", "-ctl", rds_ctl,"-rt",""], stdin=rec.stdout, stdout=subprocess.PIPE)
+fm = subprocess.Popen(["pi_fm_adv","--wait","0","--ps", "","--freq" , freq, "--audio", "-", "--ctl", rds_ctl,"--rt",""], stdin=rec.stdout, stdout=subprocess.PIPE)
 rec.stdout.close()
 output = fm.communicate()[0]
 print(output)
