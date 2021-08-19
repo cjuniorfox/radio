ed2d113a3aedbad4870a515325b5d51fe9d4e1bb
Carlos Junior <cjuniorfox@gmail.com>
2021-08-18 23:02:42 -0300

    Workaround on FM radio

--- fm.py
+++ fm.py
@@ -7,16 +7,16 @@ freq="91.5"
 ps="VROCK FM"
 rt="RADIOVROCK FM"
 
-sleephours=8
+sleephours=2
 
 #tmpdir = tempfile.mkdtemp()
 rds_ctl = os.path.join('/tmp','rds_ctl')
 subprocess.Popen(["mkfifo",rds_ctl])
 
-#FM inside while loop because this bug: https://github.com/miegl/PiFmAdv/issues/40
+#FM inside while loop to workaround this bug: https://github.com/miegl/PiFmAdv/issues/40
 while(True):
     rec = subprocess.Popen(["arecord", "-fS16_LE", "-r", "44100", "-Dplughw:0,0", "-c", "2", "-", "--quiet"],stdout=subprocess.PIPE)
-    fm = subprocess.Popen(["pi_fm_adv","--wait","0","--ps", ps,"--freq" , freq, "--audio", "-", "--ctl", rds_ctl,"--rt",rt,"--dev","85.0","--mpx","60","--preemph","us", "--cutoff","140000"], stdin=rec.stdout, stdout=subprocess.PIPE)
+    fm = subprocess.Popen(["pi_fm_adv","--wait","0","--ps", ps,"--freq" , freq, "--audio", "-", "--ctl", rds_ctl,"--rt",rt,"--mpx","40","--preemph","us", "--cutoff","150000"], stdin=rec.stdout, stdout=subprocess.PIPE)
     #print(fm.communicate()[0])
     rec.stdout.close()
     time.sleep(60*60*sleephours)
--- fm.sh
+++ fm.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
 cd /srv/media/radio && python fm.py && echo $!>/tmp/fm.pid
-#while :; do
-#        cd /srv/media/radio && python fm.py &
-#        pid=$!
-#	echo $1>/tmp/fm.pid
-#        sleep 10s
-#        kill  $pid
-#	sleep 2s
-#done
